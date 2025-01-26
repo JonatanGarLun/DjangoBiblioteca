759bcfe2fcac288588162f411f1006d0add084ef
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-01-26 19:39:11 +0100

    Terminar login

--- DjangoBiblioteca/urls.py
+++ DjangoBiblioteca/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from books import views
+from books.views import login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
