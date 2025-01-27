0f73e770509c7e7f7a2345310f646d604739fff2
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-01-27 23:44:35 +0100

    Ultimo repaso antes del examen, que sea lo que dios quiera

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
 from books.models import Book, Author, Genre
@@ -5,6 +6,7 @@ from books.models import Book, Author, Genre
 
 # Create your views here.
 
+@login_required
 def booklist(request):
     latest_books = Book.objects.order_by('-publish_date')[:5]
     return render(request, template_name= "book_list.html", context={"latest_books":latest_books})
@@ -13,19 +15,18 @@ def bookdetails(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, template_name= "book_details.html", context={"book":book})
 
+@login_required
 def authorlist(request):
     latest_authors = Author.objects.order_by('-birth_date')[:5]
     return render(request, template_name="author_list.html", context={"latest_authors":latest_authors})
 
+
+@login_required
 def authordetails(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
     return render(request, template_name="author_details.html", context={"author":author})
 
+@login_required
 def genrelist(request):
     latest_genres = Genre.objects.all()
     return render(request, template_name="genre_list.html", context={"latest_genres":latest_genres})
-
-def login():
-    pass
-def logout():
-    pass
